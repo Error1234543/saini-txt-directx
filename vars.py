8b52a63e5c0144d2ec3e3af07a0428041e60822a
Error1234543 <ptaviyad21@gmail.com>
2025-07-14 13:24:13 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+API_ID = int(environ.get("API_ID", "20619533"))
+API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8033969537:AAGHJKDLsyUSjbr4P8537w9BgT18jfr3X7I")
+OWNER = int(environ.get("OWNER", "7447651332"))
+CREDIT = environ.get("CREDIT", "SONIC")
+AUTH_USER = os.environ.get('AUTH_USERS', '7447651332').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
